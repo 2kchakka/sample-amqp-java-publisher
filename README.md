755f8a8691e3b790d7c794853e4b1404b26c1ac5
codemonkey84 <anindyabolchi@gmail.com>
2015-04-18 02:05:19 +0530

    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # sample-amqp-java-publisher
-Sample java client acting as a publisher leveraging RabbitMQ server
+Sample java client acting as a publisher leveraging RabbitMQ server. This sample project is built using TDD (Test Driven Development) approach (though the test cases are fake as of now).
+
+Clone the project
+=================
+git clone https://github.com/2kchakka/sample-amqp-java-publisher.git <project_dir>
+
+How to build
+============
+Build using Apache Maven. The command is: 
+cd <project_dir>
+mvn clean install
+
+How to run
+==========
+Run as a standalone java program. The command is:
+cd <project_dir>
+java -jar target\amqp-publisher-0.0.1-SNAPSHOT-jar-with-dependencies.jar (for linux)
+java -jar target/amqp-publisher-0.0.1-SNAPSHOT-jar-with-dependencies.jar (for windows)
+
+
+ 
---
+++ src/main/java/org/_2kchakka/amqp/Publisher.java
@@ -0,0 +1,38 @@
+package org._2kchakka.amqp;
+
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+
+/**
+ * Publisher class responsible for sending message to a queue
+ *
+ */
+public class Publisher {
+
+  private final static String QUEUE_NAME = "hello";
+
+  public static void main(String[] args)
+      throws java.io.IOException {
+      
+	  // Creating connection to RabbitMQ server
+	  ConnectionFactory factory = new ConnectionFactory();
+	  factory.setHost("localhost");
+	  Connection connection = factory.newConnection();
+	  
+	  // Creating channel - most of the API are here
+	  Channel channel = connection.createChannel();
+	  
+	  // Declaring a queue - idempotent, i.e. created iff not already created
+	  channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+      String message = "Hello World!";
+	  
+	  // Publishing a message
+      channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+      System.out.println(" [x] Sent '" + message + "'");
+	  
+	  // Closing channel & connection
+	  channel.close();
+	  connection.close();
+  }
+}
